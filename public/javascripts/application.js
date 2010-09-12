0a5fcd3833c8092e1dba832cf2e2b01029a1a5d6
Steven Talcott Smith <steve@talcottsystems.com>
2010-09-12 00:15:06 -0400

    Animate row size to make room for form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,10 +5,16 @@
 $(document).ready(function(){
 	
 	$(".bidder_name").focusin(function() {
+		$(this).parents('.row').animate({
+		    height: '153'
+		  }, 500);
 		$(this).parent().children('.hidden-elements').show(500);
 	});
 	
 	$(".item_offer").focusin(function() {
+		$(this).parents('.row').animate({
+		    height: '50'
+		  }, 500);
 		$(this).parents('.row').find('.hidden-elements').each(function() {
 			$(this).hide(500);
 		});
